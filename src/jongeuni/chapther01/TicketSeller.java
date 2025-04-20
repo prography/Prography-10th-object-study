bef63ed624532ec6fabe6c9e97ddf42b1b575e53
jongeuni <lje0818ovo@naver.com>
2025-04-20 23:17:28 +0900

    fix: TicketOffice의 자율성 빼앗기

--- src/jongeuni/chapther01/TicketSeller.java
+++ src/jongeuni/chapther01/TicketSeller.java
@@ -9,6 +9,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        ticketOffice.sellTicketTo(audience);
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }

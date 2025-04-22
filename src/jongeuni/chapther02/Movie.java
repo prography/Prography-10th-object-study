1a3430d19c26a9702d2e7083bf2ff7adbe1e71b1
jongeuni <lje0818ovo@naver.com>
2025-04-22 10:54:42 +0900

    add: Movie에 DiscountPolicy 변경할 수 있는 메서드 추가

--- src/jongeuni/chapther02/Movie.java
+++ src/jongeuni/chapther02/Movie.java
@@ -25,4 +25,8 @@ public class Movie {
         }
         return fee.minus(discountPolicy.calculateDiscountAmount(screening));
     }
+
+    public void changeDiscountPolicy(DefaultDiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 }

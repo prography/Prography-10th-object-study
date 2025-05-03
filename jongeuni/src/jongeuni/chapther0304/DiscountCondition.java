26ef4b25cbcc25cc63cad2a04a31f23f20e39ab7
jongeuni <lje0818ovo@naver.com>
2025-05-03 19:49:44 +0900

    fix: 데이터 중심 설계 개선 - 제어 객체에 밀집된 책임 분산

--- jongeuni/src/jongeuni/chapther0304/DiscountCondition.java
+++ jongeuni/src/jongeuni/chapther0304/DiscountCondition.java
@@ -6,14 +6,32 @@ import lombok.Setter;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
-@Getter
-@Setter
 public class DiscountCondition {
     private DiscountConditionType type;
-
     private int sequence;
-
     private DayOfWeek dayOfWeek;
     private LocalTime startTime;
     private LocalTime endTime;
+
+    public DiscountConditionType getType() {
+        return type;
+    }
+
+    public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+        if(type != DiscountConditionType.PERIOD) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) &&
+                !this.startTime.isAfter(time) &&
+                this.endTime.isAfter(time);
+    }
+
+    public boolean isDiscountable(int sequence) {
+        if (type!=DiscountConditionType.SEQUENCE) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.sequence == sequence;
+    }
 }
---
+++ jongeuni/src/jongeuni/chapther0304/Rectangle.java
@@ -0,0 +1,13 @@
+package jongeuni.chapther0304;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Rectangle {
+    private int left;
+    private int top;
+    private int right;
+    private int bottom;
+}

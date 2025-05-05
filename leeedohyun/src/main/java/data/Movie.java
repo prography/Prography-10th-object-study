a0e5798f49616a6d492c0920bb20ff1e96272f42
leeedohyun <ldh19738@gmail.com>
2025-05-05 16:23:29 +0900

    feat: getter/setter 생성

--- leeedohyun/src/main/java/data/Movie.java
+++ leeedohyun/src/main/java/data/Movie.java
@@ -16,4 +16,44 @@ public class Movie {
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DisCountCondition> getDisCountConditions() {
+        return disCountConditions;
+    }
+
+    public void setDisCountConditions(List<DisCountCondition> disCountConditions) {
+        this.disCountConditions = disCountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 }

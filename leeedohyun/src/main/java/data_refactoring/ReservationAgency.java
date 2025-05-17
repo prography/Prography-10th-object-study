09e4ac0bd1ff66c4d164f720d283e1ae52614dc2
leeedohyun <ldh19738@gmail.com>
2025-05-17 16:42:37 +0900

    refactor: ReservationAgency를 자율적으로 만들자

--- leeedohyun/src/main/java/data_refactoring/ReservationAgency.java
+++ leeedohyun/src/main/java/data_refactoring/ReservationAgency.java
@@ -12,24 +12,7 @@ public class ReservationAgency {
 
     private boolean checkDiscountable(Screening screening) {
         return screening.getMovie().getDisCountConditions().stream()
-                .anyMatch(condition -> isDiscountable(condition, screening));
-    }
-
-    private boolean isDiscountable(DiscountCondition condition, Screening screening) {
-        if (condition.getType() == DiscountConditionType.PERIOD) {
-            return isSatisfiedByPeriod(condition, screening);
-        }
-        return isSatisfiedBySequence(condition, screening);
-    }
-
-    private boolean isSatisfiedByPeriod(DiscountCondition condition, Screening screening) {
-        return screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
-                condition.getStartTime().compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
-                condition.getEndTime().compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
-    }
-
-    private boolean isSatisfiedBySequence(DiscountCondition condition, Screening screening) {
-        return condition.getSequence() == screening.getSequence();
+                .anyMatch(condition -> condition.isDiscountable(screening));
     }
 
     private Money calculateFee(Screening screening, boolean discountable, int audienceCount) {

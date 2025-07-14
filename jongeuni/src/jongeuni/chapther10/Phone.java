9be29a5b0642eed5992f1ff6a564c352eb1551c7
jongeuni <lje0818ovo@naver.com>
2025-07-15 00:46:35 +0900

    feat: 첫번째 요구사항

---
+++ jongeuni/src/jongeuni/chapther10/Phone.java
@@ -0,0 +1,33 @@
+package jongeuni.chapther10;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import jongeuni.chapther02.Money;
+import lombok.Getter;
+
+@Getter
+public class Phone {
+    private Money amount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<Call>();
+
+    public Phone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for(Call call : calls) {
+            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+        }
+
+        return result;
+    }
+}

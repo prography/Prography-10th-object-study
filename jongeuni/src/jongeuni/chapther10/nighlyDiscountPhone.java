a882233ba59e086bbe2b59000b534cd240addc5f
jongeuni <lje0818ovo@naver.com>
2025-07-15 00:48:52 +0900

    feat: 심야 시간에 요금을 할인해야 한다

---
+++ jongeuni/src/jongeuni/chapther10/nighlyDiscountPhone.java
@@ -0,0 +1,42 @@
+package jongeuni.chapther10;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import jongeuni.chapther02.Money;
+import lombok.Getter;
+
+@Getter
+public class nighlyDiscountPhone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<Call>();
+
+    public nighlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for(Call call : calls) {
+            if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            }
+
+        }
+
+        return result;
+    }
+}

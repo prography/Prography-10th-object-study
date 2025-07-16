32aa5bc2f2f7d06262d87b0f255a029d5ec7fedd
leeedohyun <ldh19738@gmail.com>
2025-07-16 19:36:46 +0900

    feat: 다형성 lecture

---
+++ leeedohyun/src/main/java/lecture/Lecture.java
@@ -0,0 +1,50 @@
+package lecture;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lecture {
+    private int pass;
+    private String title;
+    private List<Integer> scores = new ArrayList<>();
+
+    public Lecture(int pass, String title, List<Integer> scores) {
+        this.pass = pass;
+        this.title = title;
+        this.scores = scores;
+    }
+
+    public double average() {
+        return scores.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+    }
+
+    public List<Integer> getScores() {
+        return Collections.unmodifiableList(scores);
+    }
+
+    public String evaluate() {
+        return String.format("Pass: %d Fail: %d", passCount(), failCount());
+    }
+
+    public String stats() {
+        return String.format("Title: %s Evaluation Method: %s", title, getEvaluationMethod());
+    }
+
+    public String getEvaluationMethod() {
+        return "Pass or Fail";
+    }
+
+    private long passCount() {
+        return scores.stream()
+                .filter(score -> score >= pass)
+                .count();
+    }
+
+    private long failCount() {
+        return scores.size() - passCount();
+    }
+}
